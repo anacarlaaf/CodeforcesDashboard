de364ec5758d485e409eead298333fbc382ede43
anacarlaaf <ana.fernandes@icomp.ufam.edu.br>
2026-06-08 10:34:48 -0400

    Remove pastas ignoradas do repositório

--- teste.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_parquet("data/cses_all.parquet")
-print(df.sort_values("time").tail(10))
-print(f"\nTotal: {len(df)} registros")
-
-print(df[df["user"] == "anacarlaaf"].sort_values("time").tail(5)[["user", "problem_code", "time"]])
